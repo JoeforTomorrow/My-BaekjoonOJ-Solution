97434d284b59c94f0e8f2f29afa0d81b77660932
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2022-05-15 15:51:02 +0900

    [Silver IV] Title: 숫자 카드, Time: 2788 ms, Memory: 110172 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,18 @@
+def binary_search(array,target,start,end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+int(input())
+lst1 = sorted(list(map(int,input().split())))
+int(input())
+lst2 = list(map(int,input().split()))
+
+for i in lst2:
+    print(1, end = ' ') if binary_search(lst1,i,0,len(lst1)-1) != None else print(0, end = ' ')
