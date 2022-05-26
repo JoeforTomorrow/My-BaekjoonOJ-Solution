1b0df4dc26a80a3a2bb0c32128364cafd4569679
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2022-05-26 23:38:09 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 264 ms, Memory: 49788 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+dic={}
+for _ in range(n):
+    a,b=input().split()
+    dic[a]=b
+for _ in range(m):
+    res=input().rstrip()
+    print(dic[res])
