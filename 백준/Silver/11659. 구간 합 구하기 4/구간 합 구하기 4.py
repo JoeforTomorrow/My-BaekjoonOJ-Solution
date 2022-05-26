4970ac2c818d6a22f899275c341b4542188354d0
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2022-05-26 22:45:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 264 ms, Memory: 40596 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+n,m = map(int,input().split())
+lst = list(map(int,input().split()))
+sum_value = 0
+prefix_sum = [0]
+for i in lst:
+    sum_value += i
+    prefix_sum.append(sum_value)
+for _ in range(m):
+    left, right = map(int,input().split())
+    print(prefix_sum[right] - prefix_sum[left-1])
