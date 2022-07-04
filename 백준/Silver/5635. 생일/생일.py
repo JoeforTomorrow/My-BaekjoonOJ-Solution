845d21682ddf3381f8d5c8398c79f35cfe0276d7
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2022-07-04 20:38:29 +0900

    [Silver V] Title: 생일, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -1,5 +1,3 @@
-lst = []
-for i in range(int(input())):
-    lst.append(list(input().split()))
-lst.sort(key=lambda x:(int(x[-1]),int(x[-2]),int(x[-3])))
+lst = sorted([list(input().split()) for _ in range(int(input()))],
+      key=lambda x:(int(x[-1]),int(x[-2]),int(x[-3])))
 print(lst[-1][0],lst[0][0],sep='\n')
