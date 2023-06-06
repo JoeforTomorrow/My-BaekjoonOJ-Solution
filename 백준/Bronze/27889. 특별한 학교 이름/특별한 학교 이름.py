8a86509acf42bf44b95f79fdb40297c33c8e4249
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2023-06-06 22:58:07 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -1,5 +1,2 @@
-school = {'NLCS': 'North London Collegiate School',
-          'BHA': 'Branksome Hall Asia',
-          'KIS': 'Korea International School',
-          'SJA': 'St. Johnsbury Academy'}
-print(school[input()])
+s={'NLCS':'North London Collegiate School','BHA':'Branksome Hall Asia','KIS':'Korea International School','SJA':'St. Johnsbury Academy'}
+print(s[input()])
