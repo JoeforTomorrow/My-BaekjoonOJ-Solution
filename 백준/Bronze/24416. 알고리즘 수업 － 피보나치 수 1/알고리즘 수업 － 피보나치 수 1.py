1655299284dd239476f4cd25ca2ebe000a013982
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2024-02-27 22:45:14 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,8 +1,7 @@
 n = int(input())
-
-lst = [0] * n
-lst[0],lst[1] = 1,1
-for i in range(2,2+len(lst[2:])):
-    lst[i] = lst[i-1] + lst[i-2]
-
-print(lst[-1], n-2)
+r,a,b = 0,1,1
+for i in range(n-2):
+    r = a+b
+    a = b
+    b = r
+print(r, n-2)
