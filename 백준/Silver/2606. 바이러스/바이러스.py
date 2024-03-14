1aa44a48930b027892d65e9d5dcda0862e83bf45
JoeforTomorrow <101776866+JoeforTomorrow@users.noreply.github.com>
2024-03-14 22:10:50 +0900

    [Silver III] Title: 바이러스, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,33 +1,24 @@
-from collections import deque
-
-def bfs(graph, start, visited):
-    lst = 0
-    queue=deque([start])
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-                lst += 1
-    return lst
-
 n = int(input())
 m = int(input())
 
-visited=[False]*(n+1)
-
-graph=[[] for _ in range(n+1)]
-
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    a,b=map(int,input().split())
+    a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
+for node in graph:
+    node.sort()
+visited = [False] * (n+1)
+res = []
+def dfs(graph, start, visited, res):
+    # v = graph[start]
+    visited[start] = True
+    res.append(start)
+    for i in graph[start]:
+        # print(graph[start])
+        if not visited[i]:
+            dfs(graph, i, visited, res)
+    return res
 
-for i in range(len(graph)):
-    graph[i].sort()
-
-
-a=bfs(graph, 1, visited)
-print(a)
+r = dfs(graph,1,visited,res)
+print(len(r)-1)
